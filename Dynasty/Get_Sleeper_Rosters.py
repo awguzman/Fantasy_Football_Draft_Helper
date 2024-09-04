81478ab9dc15c432409ecfd1e202155e5f9486f4
awguzman <anthonywguzman@gmail.com>
2024-09-04 10:59:25 -0400

    Create Get_Sleeper_Rosters.py

---
+++ Dynasty/Get_Sleeper_Rosters.py
@@ -0,0 +1,30 @@
+#   Script to import Sleeper roster data after user inputs their league number. Will generate a DataFrame giving roster names per team.
+
+import pandas as pd
+import requests
+
+# turn off truncation of rows and columns if needed.
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+# Store Sleeper URL for league roster data.
+league_id = input('Enter Sleeper platform league number (This is found in the sleeper url):')
+sleeper_url = "https://api.sleeper.app/v1/league/" + league_id + "/rosters"
+
+# Request league data from Sleeper and store in a DataFrame
+sleeper_request = requests.get(sleeper_url).json()
+roster_df = pd.DataFrame.from_dict(sleeper_request, orient='columns')[['owner_id', 'players']]
+
+# Load in the player data map from Sleeper.
+map_df = pd.read_csv('Sleeper_Player_Map.csv')[['player_id', 'full_name']]
+
+# Use the sleeper player map to translate ID's into full names and create a new column listing all the players names.
+roster_df.insert(2, 'Roster', None)
+roster_df['Roster'] = roster_df['Roster'].astype(object)
+for _, row in roster_df.iterrows():
+    team_roster = []
+    for player_id in row['players']:
+        player = map_df.loc[map_df['player_id'] == player_id]
+        name = player['full_name'].tolist()[0]
+        team_roster.append(name)
+    roster_df.at[_, 'Roster'] = team_roster
