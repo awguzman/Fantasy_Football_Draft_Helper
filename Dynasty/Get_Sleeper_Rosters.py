41c78ad62c8e2468bd71262fceb55ea6aef7f65a
awguzman_work <anthonywguzman@gmail.com>
2025-08-28 12:36:23 -0400

    Switched from using sleeper API id map to nfl_data_py id map.

--- Dynasty/Get_Sleeper_Rosters.py
+++ Dynasty/Get_Sleeper_Rosters.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import requests
+import nfl_data_py as nfl
 
 # turn off truncation of rows and columns if needed.
 pd.set_option('display.max_rows', None)
@@ -13,19 +14,21 @@ sleeper_url = "https://api.sleeper.app/v1/league/" + league_id + "/rosters"
 
 # Request league data from Sleeper and store in a DataFrame
 sleeper_request = requests.get(sleeper_url).json()
-roster_df = pd.DataFrame.from_dict(sleeper_request, orient='columns')
-roster_df = roster_df[['owner_id', 'players']]
+rosters_df = pd.DataFrame.from_dict(sleeper_request, orient='columns')
+rosters_df = rosters_df[['owner_id', 'players']]
+rosters_df = rosters_df.rename(columns={'players': 'sleeper_ids'})
 
 # Load in the player data map from Sleeper.
-map_df = pd.read_csv('Sleeper_Player_Map.csv')[['player_id', 'team', 'full_name']]
+# map_df = pd.read_csv('Sleeper_Player_Map.csv')[['player_id', 'team', 'full_name']]
+map_df = nfl.import_ids()[['name', 'sleeper_id', 'fantasypros_id']]
 
 # Use the sleeper player map to translate ID's into full names and create a new column listing all the players names.
-roster_df.insert(2, 'Roster', None)
-roster_df['Roster'] = roster_df['Roster'].astype(object)
-for _, row in roster_df.iterrows():
+rosters_df.insert(2, 'fantasypros_ids', None)
+rosters_df['fantasypros_ids'] = rosters_df['fantasypros_ids'].astype(object)
+
+for _, row in rosters_df.iterrows():
     team_roster = []
-    for player_id in row['players']:
-        player = map_df.loc[map_df['player_id'] == player_id]
-        name = player['full_name']
-        team_roster.append(name.to_string(index=False))
-    roster_df.at[_, 'Roster'] = team_roster
+    for player_id in row['sleeper_ids']:
+        index = map_df.loc[map_df['sleeper_id'] == int(player_id)]
+        team_roster.append(index['fantasypros_id'].values[0])
+    rosters_df.at[_, 'fantasypros_ids'] = team_roster
